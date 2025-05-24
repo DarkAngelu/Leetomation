6d142b70e0d027c124f652ee542d63888fbf30bd
DarkAngelu <iamdarkangelu@gmail.com>
2025-05-24 17:47:20 +0530

    Added Clicking the Daily Problem Button

---
+++ utils/No_3_click_daily_problem.py
@@ -0,0 +1,46 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+
+
+
+def click_daily_problem(wait: WebDriverWait[WebDriver]) -> None:
+    """
+    Click the daily problem button on the LeetCode page.
+
+    Args:
+        wait: The WebDriverWait instance.
+
+    Returns:
+        None.
+    """
+
+
+
+    # Click the Daily Problem button
+    try:
+        # Find the button based on the structure, ignoring the exact text in the span
+        daily_problem_button = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH,
+                "//button/a[span]"
+            ))
+        )
+
+        # This sleep() is required for the daily problem button to load
+        # ; will fix later  
+        time.sleep(2)
+
+        # Click the button
+        daily_problem_button.click()
+        print("Clicked Daily Problem button.")
+    except Exception as e:
+        print("Could not click Daily Problem button:", e)
+        exit(1)
+
+    # Wait for the daily problem page to load
+    time.sleep(5)
--- utils/leetcode.py
+++ utils/leetcode.py
@@ -6,6 +6,7 @@ import time
 
 from utils.No_1_driver_and_wait_setup import driver_and_wait_setup
 from utils.No_2_add_cookies import add_cookies
+from utils.No_3_click_daily_problem import click_daily_problem
 
 
 def do_daily(cookies: Dict[str, str] = {}) -> None:
@@ -26,5 +27,11 @@ def do_daily(cookies: Dict[str, str] = {}) -> None:
     driver, wait = driver_and_wait_setup()
 
 
+
     # 2. Add cookies to the driver and navigate to the /problemset/all/ page
     add_cookies(driver, cookies)
+
+
+
+    # 3. Wait for the daily problem to be clickable and click it
+    click_daily_problem(wait)
